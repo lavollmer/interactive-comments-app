2ff04bc459b115cc146d225cf6c92ab2afb2b641
Laura <laura.e.vollmer@gmail.com>
2024-09-25 09:58:57 -0500

    updating comment card reusable component

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import IconPlus from "../assets/icon-plus.svg";
+import IconMinus from "../assets/icon-minus.svg";
+import IconReply from "../assets/icon-reply.svg";
 
 const CommentCard = () => {
   return (
-    <div>CommentCard</div>
-  )
-}
+    <div className="flex flex-row items-center font-rubik bg-white">
+      {/* increment and decrement button */}
+      <div>
+        <p>Up Down</p>
+      </div>
+      {/* name, avatar image, reply button and comment */}
+      <div>
+        <div>
+          <h1>NAME</h1>
+          <button>REPLY</button>
+        </div>
+        <div>
+          <p>COMMENT</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default CommentCard
+export default CommentCard;
