c8a7d4c508fa18e46671f5bcacf899394f951002
Laura <laura.e.vollmer@gmail.com>
2024-09-30 08:47:55 -0500

    debugging props between comment

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
+import React from 'react';
+import CommentCard from './components/CommentCard';
 import Avatar from "../assets/image-amyrobson.png";
-import CommentCard from "../components/CommentCard.jsx"
 
-const Comment = () => {
+function Comment() {
+  const commentCreatedAt = "2023-10-01T12:34:56Z"; // Example timestamp
+
   return (
     <div>
-        <CommentCard
+      <CommentCard 
         AvatarImage={Avatar}
-        AvatarDesc ="User Avatar"
-        name="amy robson"
-        comment="hello world"
-        />
+        AvatarDesc="User Avatar"
+        name="Amy Robson"
+        comment="Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well."
+        created_at={commentCreatedAt}
+      />
     </div>
-  )
+  );
 }
 
-export default Comment
+export default Comment;
--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -4,7 +4,8 @@ import IconPlus from "../assets/icon-plus.svg";
 import IconMinus from "../assets/icon-minus.svg";
 import IconReply from "../assets/icon-reply.svg";
 
-const CommentCard = ({ AvatarImage, AvatarDesc, name, comment}) => {
+
+const CommentCard = ({ AvatarImage, AvatarDesc, name, comment, created_at }) => {
   //creating a variable to hold the like count
   const [likes, setLikes] = useState(0);
 
@@ -20,12 +21,12 @@ const CommentCard = ({ AvatarImage, AvatarDesc, name, comment}) => {
     console.log(likes);
   };
 
-  // const timestamp = Date.now();
-  // const dateTimeAgo = moment(new Date(created_at)).fromNow();
-  // console.log(dateTimeAgo);
+  const timestamp = Date.now();
+  const dateTimeAgo = moment(new Date(created_at)).fromNow();
+  console.log(dateTimeAgo);
 
   return (
-    <div className="flex flex-row items-center font-rubik bg-white rounded-lg p-2 card">
+    <div className="flex flex-row items-center font-rubik bg-white rounded-lg p-2 CommentCard">
       {/* increment and decrement button */}
       <div className="flex flex-col items-center rounded-lg bg-very-light-gray m-4 p-4">
         {/* increase likes with onclick button */}
@@ -49,7 +50,7 @@ const CommentCard = ({ AvatarImage, AvatarDesc, name, comment}) => {
           <div className="flex flex-row items-center justify-center text-md space-x-4">
             <img src={AvatarImage} alt={AvatarDesc} className="h-8 w-8" />
             <h1 className="font-bold text-black">{name}</h1>
-            {/* <p>{moment(created_at).fromNow()}</p> */}
+            <p>{moment(created_at).fromNow()}</p>
           </div>
           <div>
             <button className="flex flex-row items-center font-bold text-moderate-blue text-lg">
