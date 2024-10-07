77a12e9fe27eeaea56ffc8ef2b7b8a8e995c4942
Laura <laura.e.vollmer@gmail.com>
2024-10-07 15:48:39 -0500

    debugging app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import CommentCard from "./components/CommentCard";
 import UserComment from "./components/UserComment";
-import DeleteButton from "./components/Delete"
+import DeleteButton from "./components/Delete";
 import Avatar from "./assets/image-amyrobson.png";
 
 function App() {
@@ -10,7 +10,13 @@ function App() {
     <>
       <div className="background bg-very-light-gray">
         <div className="p-10">
-          <CommentCard created_at={commentCreatedAt} AvatarDesc={AvatarDesc} AvatarImage={Avatar} />
+          <CommentCard
+            AvatarImage={Avatar}
+            AvatarDesc="User Avatar"
+            name="Amy Robson"
+            comment="Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well."
+            created_at={commentCreatedAt}
+          />
           <UserComment />
         </div>
         <div>
