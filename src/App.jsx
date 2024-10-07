b32b7c8ef7974a49d79670c142c5b45a0e5a563d
Laura <laura.e.vollmer@gmail.com>
2024-10-07 15:52:02 -0500

    added another comment card

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import CommentCard from "./components/CommentCard";
 import UserComment from "./components/UserComment";
 import DeleteButton from "./components/Delete";
 import Avatar from "./assets/image-amyrobson.png";
+import AvatarMax from "./assets/image-maxblagun.png";
 
 function App() {
   const commentCreatedAt = new Date();
@@ -10,13 +11,24 @@ function App() {
     <>
       <div className="background bg-very-light-gray">
         <div className="p-10">
-          <CommentCard
-            AvatarImage={Avatar}
-            AvatarDesc="User Avatar"
-            name="Amy Robson"
-            comment="Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well."
-            created_at={commentCreatedAt}
-          />
+          <div>
+            <CommentCard
+              AvatarImage={Avatar}
+              AvatarDesc="User Avatar"
+              name="amyrobson"
+              comment="Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well."
+              created_at={commentCreatedAt}
+            />
+          </div>
+          <div className="pt-10">
+            <CommentCard
+              AvatarImage={AvatarMax}
+              AvatarDesc="User Avatar"
+              name="maxblagun"
+              comment="Woah! Your project looks awesome."
+              created_at={commentCreatedAt}
+            />
+          </div>
           <UserComment />
         </div>
         <div>
