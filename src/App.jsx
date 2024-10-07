46b83289560c99558b76547b67b053372ae6a486
Laura <laura.e.vollmer@gmail.com>
2024-10-07 16:01:16 -0500

    working on debugging the comments section and delete button

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import React, { useState } from "react";
 import CommentCard from "./components/CommentCard";
 import UserComment from "./components/UserComment";
 import DeleteButton from "./components/Delete";
@@ -6,7 +7,17 @@ import Avatar from "./assets/image-amyrobson.png";
 import AvatarMax from "./assets/image-maxblagun.png";
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
   const commentCreatedAt = new Date();
+
+  const handleDeleteClick = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
   return (
     <>
       <div className="background bg-very-light-gray">
@@ -25,14 +36,24 @@ function App() {
               AvatarImage={AvatarMax}
               AvatarDesc="User Avatar"
               name="maxblagun"
-              comment="Woah! Your project looks awesome."
+              comment="Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well."
               created_at={commentCreatedAt}
             />
           </div>
           <UserComment />
         </div>
         <div>
-          <DeleteButton />
+          <DeleteButton show={showModal} onClose={handleCloseModal}>
+            <h2>Are you sure you want to delete this comment?</h2>
+            <button onClick={handleCloseModal}>Cancel</button>
+            <button
+              onClick={() => {
+                /* Add delete logic here */
+              }}
+            >
+              Delete
+            </button>
+          </DeleteButton>
         </div>
       </div>
     </>
