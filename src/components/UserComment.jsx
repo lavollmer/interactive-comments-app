5e12767ad897b6c676842b75ee95cd1e9046f7c5
Laura <laura.e.vollmer@gmail.com>
2024-10-09 10:08:40 -0500

    debugging send button and adding comment

--- src/components/UserComment.jsx
+++ src/components/UserComment.jsx
@@ -7,32 +7,33 @@ const UserComment = ({onAddComment}) => {
     event.preventDefault();
     const data = new FormData(event.target);
     const value = data.get("commentUser");
-    console.log(value);
+    onAddComment(value);
+    event.target.reset(); // Clear the input field after submission
   }
+
 
   return (
     <div className="flex flex-row items-center font-rubik bg-white rounded-lg p-2 mt-10">
-      {/* name, avatar image, reply button and comment */}
-      <div className="flex flex-row justify-evenly space-x-20 p-4">
-        <div className="flex flex-row items-center justify-between">
-          <div className="flex flex-row items-center justify-center text-md space-x-4">
-            <img src={Avatar} alt="Amy Robson Image" className="h-8 w-8" />
-          </div>
+    <div className="flex flex-row justify-evenly space-x-20 p-4">
+      <div className="flex flex-row items-center justify-between">
+        <div className="flex flex-row items-center justify-center text-md space-x-4">
+          <img src={Avatar} alt="User Avatar" className="h-8 w-8" />
         </div>
-        <form onSubmit={handleSubmit}>
-          <div className="outline outline-very-light-gray rounded-lg focus:outline-2 focus:border-moderate-blue">
-            <input
-              className="text-lg text-grayish-blue font-semi-bold pr-2"
-              name="commentUser"
-            />
-          </div>
-
-          <div>
-            <SendButton onClick={handleSubmit} />
-          </div>
-        </form>
       </div>
+      <form onSubmit={handleSubmit}>
+        <div className="outline outline-very-light-gray rounded-lg focus:outline-2 focus:border-moderate-blue">
+          <input
+            className="text-lg text-grayish-blue font-semi-bold pr-2"
+            name="commentUser"
+            placeholder="Add a comment..."
+          />
+        </div>
+        <div>
+          <SendButton type="submit" />
+        </div>
+      </form>
     </div>
+  </div>
   );
 };
 
