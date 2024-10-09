4f633093058b785b2ae50063a90dea5b45b678c3
Laura <laura.e.vollmer@gmail.com>
2024-10-09 10:28:13 -0500

    styling the send button and usercomment component

--- src/components/UserComment.jsx
+++ src/components/UserComment.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Avatar from "../assets/image-juliusomo.png";
 
-
 const UserComment = ({ onAddComment }) => {
   function handleSubmit(event) {
     event.preventDefault();
@@ -12,19 +11,21 @@ const UserComment = ({ onAddComment }) => {
   }
 
   return (
-    <div className="flex flex-row font-rubik bg-white rounded-lg p-2 mt-10 w-full">
+    <div className="flex flex-row  items-center space-x-10 font-rubik bg-white rounded-lg p-6 mt-10 w-full">
       <img src={Avatar} alt="User Avatar" className="h-8 w-8" />
 
-      <form onSubmit={handleSubmit}>
-        <div className="outline outline-very-light-gray rounded-lg focus:outline-2 focus:border-moderate-blue">
-          <input
-            className="text-lg text-grayish-blue font-semi-bold pr-2"
-            name="commentUser"
-            placeholder="Add a comment..."
-          />
-        </div>
-        <button type="submit" className='flex flex-row items-center justify-center text-lg bg-moderate-blue rounded-lg px-8 py-4 text-white font-bold font-rubik'>
-           SEND
+      <form onSubmit={handleSubmit} className="flex flex-row items-center space-x-4">
+        <input
+          className="outline outline-very-light-gray rounded-lg focus:outline-2 focus:border-moderate-blue text-lg text-grayish-blue font-semi-bold pr-2"
+          name="commentUser"
+          placeholder="Add a comment..."
+        />
+
+        <button
+          type="submit"
+          className="flex flex-row items-center justify-center text-md bg-moderate-blue rounded-lg px-4 py-2 text-white font-bold font-rubik"
+        >
+          SEND
         </button>
       </form>
     </div>
