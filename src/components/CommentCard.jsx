44800b2562006827e1b4555e0bd84a6c10a09684
Laura <laura.e.vollmer@gmail.com>
2024-12-02 15:34:02 -0600

    added the you tag next to the user when commenting

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -52,81 +52,88 @@ const CommentCard = ({
     // checks that the reply is not empty
     if (replyText.trim()) {
       // calls the onAddReply function and passes the reply text
-     onAddReply(replyText);
-     setReplyText("");
+      onAddReply(replyText);
+      setReplyText("");
     }
   };
 
   console.log(replies);
 
   return (
-      <div className="flex flex-row justify-center bg-white rounded-lg w-full mt-8">
-        {/* increment and decrement button */}
-        <div className="flex flex-col items-center rounded-lg bg-very-light-gray m-4 p-4">
-          {/* increase likes with onclick button */}
-          <button title="increase" onClick={increaseLikes}>
-            <img
-              src={IconPlus}
-              alt="Increment Icon"
-              className="font-bold text-moderate-blue pb-2"
-            />
-          </button>
-          {/* displaying the likes based on the state */}
-          <p className="text-moderate-blue font-bold text-lg pb-4">{likes}</p>
-          {/* decreasing likes based onclick */}
-          <button title="decrease" onClick={decreaseLikes}>
-            <img src={IconMinus} alt="Decrement Icon" className=" font-bold" />
-          </button>
-        </div>
+    <div className="flex flex-row justify-center bg-white rounded-lg w-full mt-8">
+      {/* increment and decrement button */}
+      <div className="flex flex-col items-center rounded-lg bg-very-light-gray m-4 p-4">
+        {/* increase likes with onclick button */}
+        <button title="increase" onClick={increaseLikes}>
+          <img
+            src={IconPlus}
+            alt="Increment Icon"
+            className="font-bold text-moderate-blue pb-2"
+          />
+        </button>
+        {/* displaying the likes based on the state */}
+        <p className="text-moderate-blue font-bold text-lg pb-4">{likes}</p>
+        {/* decreasing likes based onclick */}
+        <button title="decrease" onClick={decreaseLikes}>
+          <img src={IconMinus} alt="Decrement Icon" className=" font-bold" />
+        </button>
+      </div>
 
-        <div className="flex flex-col space-y-4 p-4 w-full">
-          <div className="flex flex-row items-center justify-between">
-            <div className="flex items-center space-x-4">
-              <img src={AvatarImage} alt={AvatarDesc} className="h-8 w-8" />
-              <h1 className="font-bold text-black">{name}</h1>
-              <p>{moment(created_at).fromNow()}</p>
-            </div>
-            <div className="flex items-center space-x-4">
-              {currentUser.username === name && (
-                <button className="text-red-500 font-bold" onClick={onDelete}>
+      <div className="flex flex-col space-y-4 p-4 w-full">
+        <div className="flex flex-row items-center justify-between">
+          <div className="flex items-center space-x-4">
+            <img src={AvatarImage} alt={AvatarDesc} className="h-8 w-8" />
+            <h1 className="font-bold text-black">{name}</h1>
+            <p className="text-grayish-blue">{moment(created_at).fromNow()}</p>
+            {currentUser.username === name && (
+              <div className="flex flex-row space-x-2">
+                <p className="py-1 px-2 font-bold bg-moderate-blue rounded-lg text-white text-sm">
+                  you
+                </p>
+                <button
+                  className="flex flex-row pr-2 items-center text-red-500 font-bold"
+                  onClick={onDelete}
+                >
                   <img src={IconDelete} alt="Delete Icon" className="h-4 w-4" />
                   Delete
                 </button>
-              )}
-              <button
-                className="flex flex-row items-center justify-center text-moderate-blue font-bold"
-                onClick={handleReplyClick}
-              >
-                <img src={IconReply} alt="Reply Icon" className="pr-2" />
-                Reply
-              </button>
-            </div>
+              </div>
+            )}
+            <button
+              className="flex flex-row items-center justify-center text-moderate-blue font-bold"
+              onClick={handleReplyClick}
+            >
+              <img src={IconReply} alt="Reply Icon" className="pr-2" />
+              Reply
+            </button>
           </div>
-          <div>
-            <p className="text-lg text-grayish-blue font-semi-bold pr-2">
-              {comment}
-            </p>
+        </div>
+        <div>
+          <p className="text-lg text-grayish-blue font-semi-bold pr-2">
+            {comment}
+          </p>
+        </div>
+        {/* When the user selects the reply button - the reply box displays */}
+        {showReplyBox && (
+          <div className="reply-box">
+            <textarea
+              value={replyText}
+              onChange={(e) => setReplyText(e.target.value)}
+              placeholder="Write your reply..."
+              className="w-full p-2 border rounded"
+            />
+            {/* Reply button */}
+            <button
+              onClick={handleReplySubmit}
+              className="flex flex-row items-center justify-center text-lg bg-moderate-blue rounded-lg px-4 py-4 text-white font-bold font-rubik"
+            >
+              Reply
+            </button>
           </div>
-          {/* When the user selects the reply button - the reply box displays */}
-          {showReplyBox && (
-            <div className="reply-box">
-              <textarea
-                value={replyText}
-                onChange={(e) => setReplyText(e.target.value)}
-                placeholder="Write your reply..."
-                className="w-full p-2 border rounded"
-              />
-              {/* Reply button */}
-              <button
-                onClick={handleReplySubmit}
-                className="flex flex-row items-center justify-center text-lg bg-moderate-blue rounded-lg px-4 py-4 text-white font-bold font-rubik"
-              >
-                Reply
-              </button>
-            </div>
-          )}
-          <div className="replies mt-4">
-            {replies.length > 0 && replies.map((reply, index) => (
+        )}
+        <div className="replies mt-4">
+          {replies.length > 0 &&
+            replies.map((reply, index) => (
               <ReplyCard
                 key={index}
                 AvatarImage={reply.avatar}
@@ -136,9 +143,9 @@ const CommentCard = ({
                 createdAt={reply.createdAt}
               />
             ))}
-            </div>
         </div>
       </div>
+    </div>
   );
 };
 
