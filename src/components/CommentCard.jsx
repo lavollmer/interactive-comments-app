88ef66d9730788ebccf6bf4165e3c22b52c17e11
Laura <laura.e.vollmer@gmail.com>
2024-12-02 17:26:49 -0600

    updating the delete modal

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -6,10 +6,34 @@ import IconReply from "../assets/icon-reply.svg";
 import ReplyCard from "./ReplyCard";
 import IconDelete from "../assets/icon-delete.svg";
 import IconEdit from "../assets/icon-edit.svg";
-import DeleteModal from "../components/Delete";
 
 const Modal = ({ onClose }) => {
-  return <div>Delete Modal</div>;
+  return (
+    <div>
+      <div className="flex flex-col items-center font-rubik bg-white rounded-lg modal-overlay">
+        <div className="modal-content">
+          <h1 className="font-bold text-xl">Delete Comment</h1>
+          <p className="text-md text-grayish-blue">
+            Are you sure you want to delete this comment? This will remove the
+            comment and can't be undone.
+          </p>
+        </div>
+        <div className="flex flex-row justify-center items-center space-x-4 modal-close">
+          <div>
+            <button
+              className="bg-grayish-blue rounded-lg p-4"
+              onClick={onClose}
+            >
+              NO, CANCEL
+            </button>
+          </div>
+          <div>
+            <button className="bg-soft-red rounded-lg p-4">YES, DELETE</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 const CommentCard = ({
